c169f7cf9ef5499986f3b83dc65b91ee14c70b26
chan <cham9994@gmail.com>
2022-07-07 16:26:05 +0900

    🔥 Start!

---
+++ src/pages/Store.tsx
@@ -0,0 +1,16 @@
+import { Col, Row } from 'react-bootstrap'
+import storeItems from '../data/items.json'
+
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      {/* 화면 사이즈에 따른 컬럼 사이즈 지정 */}
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item =>(
+          <Col>{JSON.stringify(item)}</Col>
+        ))}
+      </Row>
+    </>
+  )
+}
